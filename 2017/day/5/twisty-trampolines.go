ff3be5e4d40c22672700a377e769b9a549eebd00
Robert Grant <rhgrant10@gmail.com>
2018-06-17 17:38:50 -0700

    Do day 5

---
+++ 2017/day/5/twisty-trampolines.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+import "io/ioutil"
+import "os"
+import "strings"
+import "strconv"
+
+
+func createMaze(filename string) []int {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        panic(err)
+    }
+
+    instructions := strings.Trim(string(data), "\n")
+    lines := strings.Split(instructions, "\n")
+
+    var maze []int
+    for _, line := range lines {
+        offset, err := strconv.Atoi(line)
+        if err != nil {
+            panic(err)
+        }
+        maze = append(maze, offset)
+    }
+
+    return maze
+}
+
+
+func countEscapeSteps(maze []int) (steps int) {
+    var index int
+    for 0 <= index && index < len(maze) {
+        newIndex := index + maze[index]
+        maze[index] += 1
+        index = newIndex
+        steps += 1
+    }
+    return
+}
+
+
+func main() {
+    maze := createMaze(os.Args[1])
+    count := countEscapeSteps(maze)
+    fmt.Println(count)
+}
---
+++ 2017/day/5/twisty-trampolines.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def escape(maze):
+    index = 0
+    while 0 <= index < len(maze):
+        new_index = index + maze[index]
+        maze[index] += 1
+        index = new_index
+        yield index
+
+
+def count_escape_steps(filename):
+    with open(filename) as f:
+        instructions = f.read().splitlines()
+
+    maze = [int(i) for i in instructions]
+    return len(list(escape(maze)))
+
+
+if __name__ == '__main__':
+    print(count_escape_steps(sys.argv[1])
