dc582460ff0a0a3605e8a06376456999a38d4257
Robert Grant <rhgrant10@gmail.com>
2018-06-24 07:41:11 -0700

    Day 8 part 2

---
+++ 2017/08/high-registers.py
@@ -0,0 +1,73 @@
+import sys
+import collections
+import operator
+
+
+OPERATORS = {
+    'inc': operator.add,
+    'dec': operator.sub,
+}
+
+COMPARATORS = {
+    '==': operator.eq,
+    '!=': operator.ne,
+    '<': operator.lt,
+    '>': operator.gt,
+    '<=': operator.le,
+    '>=': operator.ge,
+}
+
+
+def parse_input_file(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def parse_instruction(line):
+    operation, condition = line.split(' if ')
+    perform_operation = parse_operation(operation)
+    condition = parse_condition(condition)
+    return perform_operation, condition
+
+
+def parse_operation(operation):
+    register, symbol, operand = operation.split()
+    operand = int(operand)
+
+    def perform_operation(registers):
+        registers[register] = OPERATORS[symbol](registers[register], operand)
+
+    return perform_operation
+
+
+def parse_condition(condition):
+    register, symbol, operand = condition.split()
+    operand = int(operand)
+
+    def compare(registers):
+        return COMPARATORS[symbol](registers[register], operand)
+
+    return compare
+
+
+def execute(instructions, registers):
+    max_value = 0
+    for line in instructions:
+        perform_operation, condition = parse_instruction(line)
+        if condition(registers):
+            perform_operation(registers)
+        current_max = max(registers.values())
+        max_value = max(current_max, max_value)
+    return max_value
+
+
+def main(filename):
+    instructions = parse_input_file(filename)
+    registers = collections.defaultdict(int)
+    max_value = execute(instructions, registers)
+    print(max_value)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
