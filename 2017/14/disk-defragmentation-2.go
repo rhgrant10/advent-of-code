4eefd0e6534a363461c61a7a409ba812c0cfa489
Robert Grant <rhgrant10@gmail.com>
2018-07-15 20:33:36 -0700

    Finally! W o w z e r s

--- 2017/14/disk-defragmentation-2.go
+++ 2017/14/disk-defragmentation-2.go
@@ -1,24 +1,8 @@
 package main
 
 import "fmt"
-import "image"
-import "image/color"
-import "image/png"
-import "math"
 import "os"
 
-
-// const NOT_USED = "\u2591\u2591"
-const NOT_USED = "  "
-
-const HORIZONTAL = "\u2501\u2501"
-const LEFT_VERTICAL = " \u2503"
-const RIGHT_VERTICAL = "\u2503"
-
-const TOP_LEFT_CORNER = " \u250F"
-const TOP_RIGHT_CORNER = "\u2513"
-const BOTTOM_LEFT_CORNER = " \u2517"
-const BOTTOM_RIGHT_CORNER = "\u251B"
 
 const NUM_KNOTS = 256
 const NUM_KEYS = 128
@@ -151,208 +135,45 @@ func getDiskState(keyString string) map[Coord]bool {
         var knotHash = getKnotHash(rowKey)
         for x, bit := range getBits(knotHash) {
             var coord = Coord{x, y}
-            grid[coord] = bit != 0
+            grid[coord] = bit == '1'
         }
     }
     return grid
 }
 
 
-func getBits(knotHash []byte) (bits []byte) {
+func getBits(knotHash []byte) (bits string) {
     for _, knot := range knotHash {
-        var mask byte = 1
-        for i := 0; i < 8; i++ {
-            var bit = knot & mask
-            bits = append(bits, bit)
-            mask = mask << 1
-        }
+        bits += fmt.Sprintf("%08b", knot)
     }
     return
 }
 
 
-func countIslands(grid map[Coord]bool) (int, map[Coord]int) {
-    var islands = make(map[Coord]int, 0)
+func countIslands(grid map[Coord]bool) (count int) {
+    var visited = make(map[Coord]bool, 0)
 
-    var count int = 0
     for coord, isUsed := range grid {
-        if !isUsed || islands[coord] > 0 {
+        if !isUsed || visited[coord] {
             continue
         }
 
         count++
         var stack CoordStack
         stack.Push(coord)
-        islands[coord] = count
+        visited[coord] = true
 
         for !stack.IsEmpty() {
             coord = stack.Pop()
             for _, neighbor := range getConnectedNeighbors(coord) {
-                if islands[neighbor] == 0 && grid[neighbor] {
+                if !visited[neighbor] && grid[neighbor] {
                     stack.Push(neighbor)
                 }
-                islands[coord] = count
+                visited[neighbor] = true
             }
         }
     }
-    return count, islands
-}
-
-
-func printGrid(grid map[Coord]bool, start Coord, end Coord) {
-    for y := start[1]; y <= end[1]; y++ {
-        for x := start[0]; x <= end[0]; x++ {
-            var coord = Coord{x, y}
-            if isInbounds(coord) {
-                if grid[coord] {
-                    fmt.Print("#")
-                } else {
-                    fmt.Print(".")
-                }
-            } else {
-                fmt.Print(string(rune(9608)))
-            }
-        }
-        fmt.Println()
-    }
-}
-
-
-func printIslands(grid map[Coord]int, start Coord, end Coord) {
-    for y := start[1]; y <= end[1]; y++ {
-        for x := start[0]; x <= end[0]; x++ {
-            var coord = Coord{x, y}
-            if isInbounds(coord) {
-                var index = grid[coord]
-                if index > 0 {
-                    fmt.Print(printable(index))
-                } else {
-                    fmt.Print(NOT_USED)
-                }
-            } else {
-                fmt.Print(outOfBoundsChar(coord))
-            }
-        }
-        fmt.Println()
-    }
-}
-
-
-func drawIslands(grid map[Coord]int, palette []color.Color) image.Image {
-    var image = image.NewRGBA(image.Rect(0, 0, 128, 128))
-    for y := 0; y <= 128; y++ {
-        for x := 0; x <= 128; x++ {
-            var coord = Coord{x, y}
-            var colorId = grid[coord] % len(palette)
-            image.Set(x, y, palette[colorId])
-        }
-    }
-    return image
-}
-
-
-func convertToRGB(hue float64, saturation float64, value float64) color.NRGBA {
-    var i int = int(hue * 6)
-    var f float64 = hue * 6 - float64(i)
-    var p float64 = value * (1 - saturation)
-    var q float64 = value * (1 - f - saturation)
-    var t float64 = value * (1 - (1 - f) * saturation)
-
-    var r, g, b float64
-    switch i % 6 {
-        case 0: r, g, b = value, t, p
-        case 1: r, g, b = q, value, p
-        case 2: r, g, b = p, value, t
-        case 3: r, g, b = p, q, value
-        case 4: r, g, b = t, p, value
-        case 5: r, g, b = value, p, q
-    }
-
-    var red, green, blue uint8 = uint8(255 * r), uint8(255 * g), uint8(255 * b)
-    return color.NRGBA{red, green, blue, 255}
-}
-
-
-func generatePalette(count int) (palette []color.Color) {
-    palette = append(palette, color.Black)
-
-    if count < 1 {
-        return palette
-    }
-
-    const NUM_HUES = 360
-    const MAX float64 = 1.0
-    const MIN float64 = 0.5
-    const SPREAD float64 = MAX - MIN
-
-    var totalRounds int = count / NUM_HUES + 1
-
-    var colorsPerRound int = (count + 1) / totalRounds
-    var dH float64 = MAX / float64(colorsPerRound)
-
-    var numValues int = squarestFactor(totalRounds)
-    var dV float64 = SPREAD / float64(numValues)
-
-    var numSaturations int = totalRounds / numValues
-    var dS float64 = SPREAD / float64(numSaturations)
-
-    var h, s, v float64 = 0, MIN, MIN
-    for si := 0; si < numSaturations; si++ {
-        for vi := 0; vi < numValues; vi++ {
-            for h = 0; h < MAX; h += dH {
-                var color = convertToRGB(h, s, v)
-                palette = append(palette, color)
-            }
-            v += dV
-        }
-        s += dS
-    }
-    return palette
-}
-
-
-func squarestFactor(value int) int {
-    var guess = int(math.Sqrt(float64(value)))
-    for guess > 1 {
-        if value % guess == 0 {
-            return guess
-        }
-    }
-    return value
-}
-
-
-func outOfBoundsChar(coord Coord) string {
-    var x, y = coord[0], coord[1]
-    if y < 0 {
-        if x < 0 {
-            return TOP_LEFT_CORNER
-        } else if x > 127 {
-            return TOP_RIGHT_CORNER
-        } else {
-            return HORIZONTAL
-        }
-    } else if y > 127 {
-        if x < 0 {
-            return BOTTOM_LEFT_CORNER
-        } else if x > 127 {
-            return BOTTOM_RIGHT_CORNER
-        } else {
-            return HORIZONTAL
-        }
-    } else if x < 0 {
-        return LEFT_VERTICAL
-    } else if x > 127 {
-        return RIGHT_VERTICAL
-    }
-    panic(fmt.Errorf("wat"))
-}
-
-
-func printable(index int) string {
-    const LOWER, UPPER = '\u4E00', '\u9FFF'
-    var value = index % (UPPER - LOWER + 1) + LOWER
-    return string(rune(value))
+    return
 }
 
 
@@ -378,25 +199,9 @@ func isInbounds(coord Coord) bool {
 }
 
 
-func saveImage(image image.Image, filename string) {
-    var fp, err = os.Create(filename)
-    if err != nil {
-        panic(err)
-    }
-    defer fp.Close()
-    png.Encode(fp, image)
-    fmt.Printf("Image written to %s\n", filename)
-}
-
-
 func main() {
     var keyString = os.Args[1]
     var grid = getDiskState(keyString)
-    printGrid(grid, Coord{-1, -1}, Coord{128, 128})
-    var numIslands, islands = countIslands(grid)
-    printIslands(islands, Coord{-1, -1}, Coord{128, 128})
+    var numIslands = countIslands(grid)
     fmt.Println(numIslands)
-    var palette = generatePalette(numIslands)
-    var image = drawIslands(islands, palette)
-    saveImage(image, fmt.Sprintf("%s.png", keyString))
 }
